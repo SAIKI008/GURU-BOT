775a9c84f397048b17763b252dca3931f5da6aeb
SAIKI008 <157207307+SAIKI008@users.noreply.github.com>
2024-01-28 23:50:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917605902011', 'Saiki', true],
+  ['6281255369012', 'Saiki', true], 
   [''] 
 ] //Number of owners
 
